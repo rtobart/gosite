d7fd3c0a611830d47d45bb3bedd62c3128d534f2
rtobart <81400134+rtobart@users.noreply.github.com>
2021-10-18 16:08:41 -0300

    avance robinson

--- pulls/models.py
+++ pulls/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Items(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    price = models.PositiveIntegerField
+    image = models.ImageField(upload_to='items',null=True)
